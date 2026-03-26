4507d2659521431a0c9ce1da417910e3a876e292
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2026-03-26 08:34:05 +0800

    更新 sign.py

--- sign.py
+++ sign.py
@@ -1,4 +1,11 @@
 from cloakbrowser import launch
+import argparse
+
+parser = argparse.ArgumentParser(description='Parse user data')
+parser.add_argument("--email", help="Login Email", default="")
+parser.add_argument("--password", help="Login Password", default="")
+args = parser.parse_args()
+
 
 browser = launch()
 page = browser.new_page()
